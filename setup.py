01072c7299853898f3a1dc6623140a1a81554a35
Jeremías <jrmsdev@gmail.com>
2017-07-28 20:15:43 -0300

    setuptools integration

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import sys
+from setuptools import setup
+from unittest import TestLoader
+
+desc = 'Python3 Tests Factory'
+tests_discover = TestLoader ().discover ('test', '*_t.py'),
+
+setup(
+    name = 'tf',
+    version = '0.0',
+
+    description = desc,
+    long_description = desc,
+
+    license = 'BSD',
+    url = 'https://github.com/jrmsdev/pytf',
+
+    author = 'Jeremías Casteglione',
+    author_email = 'jrmsdev@gmail.com',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+    ],
+
+    py_modules = ['tf', 'cmd', 'loader', 'suite', 'tc', 'tc_http'],
+    data_files = [
+        ('', ['LICENSE', 'README.md']),
+        ('examples', ['examples/basic.ini']),
+    ],
+    zip_safe = False,
+
+    entry_points = {
+        'console_scripts': [
+            'tf=tf:main',
+        ],
+    },
+
+    test_suite = 'test.suite',
+)
--- tf.py
+++ tf.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
-import cmd
+from unittest.runner import TextTestRunner
+from os.path import abspath
+import suite
 
-srcdir = '.'
-if len (sys.argv) > 1:
-    srcdir = sys.argv[1]
+def main (srcdir = '.', stream = None):
+    srcdir = '.'
+    if len (sys.argv) > 1:
+        srcdir = sys.argv[1]
+    s = suite.load (srcdir)
+    if len (s._tests) > 0:
+        r = TextTestRunner (stream = stream, verbosity = 2).run (s)
+        return len (r.errors) or len (r.failures)
+    else:
+        print ("%s: no tests found" % abspath (srcdir))
+        return 0
 
-sys.exit (cmd.main (srcdir = srcdir))
+if __name__ == '__main__':
+    sys.exit (main ())
